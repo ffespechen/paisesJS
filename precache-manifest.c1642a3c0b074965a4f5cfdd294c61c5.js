4ca4da57c8af365e55d66bc293c18f16ba59de40
ffespechen <flavio.espechenieva@gmail.com>
2020-07-30 13:01:33 -0300

    Updates

---
+++ precache-manifest.c1642a3c0b074965a4f5cfdd294c61c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a43bc27e4fa39471333a99ac27a7bc7",
+    "url": "/paisesJS/index.html"
+  },
+  {
+    "revision": "f7ee8d873593d65b066f",
+    "url": "/paisesJS/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "786937533d2861ebd97a",
+    "url": "/paisesJS/static/js/2.156ef906.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/paisesJS/static/js/2.156ef906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ee8d873593d65b066f",
+    "url": "/paisesJS/static/js/main.a2dc9876.chunk.js"
+  },
+  {
+    "revision": "432cf1af3f8904a5fed6",
+    "url": "/paisesJS/static/js/runtime-main.f42dcf54.js"
+  }
+]);
